28e3f8308f1bf98e9f3c1304e15130c799f01eb5
MexpressoBean <kingk102@gmail.com>
2024-09-21 13:07:12 -0500

    simplify so that a button just logs to the console on click

--- popup.js
+++ popup.js
@@ -1,17 +1,3 @@
-document.getElementById('capture-contact').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        func: extractContactInfoFromPage,
-      });
-    });
-  });
-  
-  function extractContactInfoFromPage() {
-    const emailBody = document.querySelector('.ii.gt');
-    if (emailBody) {
-      const contactInfo = extractContactInfo(emailBody.innerText);
-      document.getElementById('contact-info').innerText = JSON.stringify(contactInfo, null, 2);
-    }
-  }
-  
+document.getElementById('logButton').addEventListener('click', () => {
+  console.log('Button was clicked!');
+});
