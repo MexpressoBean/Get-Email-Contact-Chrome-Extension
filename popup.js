b600d10fb48a53d57715d379498f8abc9973c94e
MexpressoBean <kingk102@gmail.com>
2024-09-29 17:15:00 -0500

    improve the popup UI

--- popup.js
+++ popup.js
@@ -1,25 +1,38 @@
 // Event listener for button click
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("logButton").addEventListener("click", () => {
-    chrome.runtime.sendMessage({ name: "extractEmailBody" }, (response) => {
-      if (response && response.emailContent) {
-        chrome.runtime.sendMessage(
-          {
-            name: "extractContactInfoFromEmail",
-            emailContent: response.emailContent,
-            senderEmailAddress: response.senderEmailAddress,
-          },
-          (response) => {
-            console.log(
-              `RESPONSE in popup.js: ${
-                response.contactInfo
-              } && ${JSON.stringify(response.contactInfo)}`
-            );
-          }
-        );
-      } else {
-        console.log("Failed to extract email content.");
-      }
+  document
+    .getElementById("getContactInfoButton")
+    .addEventListener("click", () => {
+      chrome.runtime.sendMessage({ name: "extractEmailBody" }, (response) => {
+        if (response && response.emailContent) {
+          chrome.runtime.sendMessage(
+            {
+              name: "extractContactInfoFromEmail",
+              emailContent: response.emailContent,
+              senderEmailAddress: response.senderEmailAddress,
+            },
+            (response) => {
+              console.log(
+                `RESPONSE in popup.js: ${
+                  response.contactInfo
+                } && ${JSON.stringify(response.contactInfo)}`
+              );
+
+              // Display the contact info in the preview area
+              document.getElementById("contactInfoPreview").textContent =
+                JSON.stringify(response.contactInfo, null, 2);
+            }
+          );
+        } else {
+          console.log("Failed to extract email content.");
+        }
+      });
     });
-  });
+
+  document
+    .getElementById("createContactButton")
+    .addEventListener("click", () => {
+      // Logic for creating the contact in Google Contacts goes here
+      console.log("Create contact button clicked");
+    });
 });
