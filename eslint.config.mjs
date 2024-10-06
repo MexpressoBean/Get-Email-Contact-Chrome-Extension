a00a09ab61d0fcedb82a94f4ea0b543b2528beab
MexpressoBean <kingk102@gmail.com>
2024-10-06 10:50:43 -0500

    fix lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { languageOptions: { ...globals.browser, ...globals.webextensions } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Adds browser-specific globals like window, document, etc.
+        ...globals.webextensions, // Adds globals like 'chrome' for Chrome extensions
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
